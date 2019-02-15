3c067a651e4fada52a0e57179678925824dd4d7e
Brad Barnes <bjb211@uga.edu>
2019-02-15 17:26:11 -0500

    Added another container class

--- src/cs1302/generics/Driver.java
+++ src/cs1302/generics/Driver.java
@@ -1,13 +1,31 @@
+package cs1302.generics;
+
+/**
+ * Driver program for cs1302 class exercise 12 using various types of 
+ * container objects.
+ * 
+ * @author Brad Barnes and Supa' Mike
+ * @version 1.0
+ */
+
 public class Driver {
 
+    /**
+     * Creates objects for each type of container to check basic functionality.
+     */
     public static void main(String[] args) {
+	
+	//Declare objects of all of our container types
 	StringContainer sc = new StringContainer("I wish I had a next reference");
 	DoubleContainer dc = new DoubleContainer(4.5);
 	IntegerContainer ic = new IntegerContainer(7);
+	BooleanContainer bc = new BooleanContainer(true);
 
+	//Print the contents of each container
 	System.out.println(sc.get());
 	System.out.println(dc.get());
 	System.out.println(ic.get());
+	System.out.println(bc.get());
     } // main
     
 } // Driver
